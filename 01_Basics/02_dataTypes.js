16d65347c4517c47335a8ba12f1a4d12580cd555
Baizid Khan <77749510+baizidkhan@users.noreply.github.com>
2023-10-21 22:22:18 +0000

    Different types of Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert (3+3)  //We are using nodejs, not browser
+
+console.log(3+3)
+console.log("Baizid") //code readability should be high 
+
+
+// https://tc39.es/ecma262/#sec-intro     //ECMAScript - JavaScript Standard
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript    //Mdn - JavaScript Documentation
+
+
+
+// Data Type :----------------------------------------
+let nam = "Baizid"
+let age = 23
+let isLoggedIn = false
+let temperature = null
+let state;
+
+// number => to power 53
+// BigInt
+// String => ""
+// Boolean => true/ false
+// null => standalone value = value assigned null, not undefined
+// undefined => value not assigned
+// Symbol => To find unique in basically react
+
+
+
+// Object 
+
+console.log(typeof"Bikhan");
+console.log(typeof null);
+console.log(typeof undefined);
+console.table([nam, age, isLoggedIn, temperature])
+
+
+console.log("Hello everyone!!");
+console.log("This is my first time on online VScode.");
